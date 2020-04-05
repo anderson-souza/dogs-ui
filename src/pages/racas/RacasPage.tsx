1bdc1a2d7066fd030656bc34affb0804ea8b10d4
Anderson <andersonpds14@gmail.com>
2020-04-05 16:25:37 -0300

    Adicionado Axios; Criado página para listagem das raças;

--- src/pages/racas/RacasPage.tsx
+++ src/pages/racas/RacasPage.tsx
@@ -1,9 +1,37 @@
 import React from "react";
+import { getRacas } from "../../services/RacasService";
+import { Return } from "../../interfaces/Return";
 
 const RacasPage = () => {
+  const [racas, setracas] = React.useState([]);
+
+  async function loadRacas() {
+    await getRacas
+      .then((response: any) => {
+        console.log(response);
+
+        const { data } = response;
+
+        setracas(data.message);
+
+        console.log(typeof racas);
+      })
+      .catch((error: any) => {
+        alert(error);
+      });
+  }
+
+  React.useEffect(() => {
+    loadRacas();
+  }, []);
+
   return (
     <div>
-      <p>E num é que aqui vai ter umas raças daora</p>
+      <ul>
+        {Object.entries(racas).map((row: any) => (
+          <li key={row[0]}>{row[0]}</li>
+        ))}
+      </ul>
     </div>
   );
 };
---
+++ src/services/RacasService.tsx
@@ -0,0 +1,3 @@
+import api from "./DogsApi";
+
+export let getRacas = api.get("/breeds/list/all");
