fea6a1566caef0b0694148b3d6a5597fb5e3d9d4
Anderson <andersonpds14@gmail.com>
2020-04-05 15:14:14 -0300

    Estilizado content; Alterado alguns estilos do Header; Movido App para dentro do Routes;

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "./components/Header/header";
 import "./App.css";
 import Routes from "./routes/Routes";
 
 function App() {
   return (
     <div className='App'>
-      <Header />
       <Routes />
     </div>
   );
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import RacasPage from "../pages/racas/RacasPage";
+import Header from "../components/Header/header";
+import "./style.css";
 
 const Routes: React.SFC = () => {
   return (
     <Router>
-      <div>
+      <Header />
+      <div className='content'>
         <Route path='/' component={RacasPage} />
       </div>
     </Router>
