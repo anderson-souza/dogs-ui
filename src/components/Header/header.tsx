dbcd17d1c7cdef69721c73c30a7ecd6d0253a49c
Anderson <andersonpds14@gmail.com>
2020-04-05 13:45:34 -0300

    Criado componente de Header; Adicionado Normalize.css no projeto;

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import "./style.css";
+
+const Header: React.SFC = () => {
+  return (
+    <header>
+      <p>Sup niggas</p>
+    </header>
+  );
+};
+
+export default Header;
